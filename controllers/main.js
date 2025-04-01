7e5ba2e081bb1daae83d19697ae64d0a8481419e
shem4soul@gmail.com <shem4soul@gmail.com>
2025-04-01 14:24:19 +0100

    refactor

--- controllers/main.js
+++ controllers/main.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken')
-const CustomAPIError = require('../errors/custom-error')
+const { BadRequestError } = require('../errors')
 
 const login = async (req, res) => {
 const {username, password} = req.body
 
 if (!username || !password) {
-    throw new CustomAPIError ('please provide email and password', 400)
+    throw new BadRequestError ('please provide email and password')
 }
 // just for demo, normally provided by DB
 const id = new Date().getDate()
@@ -18,17 +18,15 @@ const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn:'30d'}
 
 
 const dashboard = async (req, res) => {
-   const authHeader = req.headers.authorization
+console.log(req.user);
 
-if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new CustomAPIError('No token provided', 401)
-}
+   const luckyNumber = Math.floor(Math.random()*100)
 
-const token = authHeader.split(' ')[1]
-console.log(token);
-        const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg: `Hello, shittu Emmanuel`, secret: `Here is ur authorized data, your lucky number is ${luckyNumber}`})
-}
+    res.status(200).json({
+        msg: `Hello, ${req.user.username}`, 
+        secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})
+} 
+
 
 
 module.exports = {
--- routes/main.js
+++ routes/main.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const { login, dashboard } = require('../controllers/main');
+const authMiddleware = require('../middleware/auth');
 
-router.get('/dashboard', dashboard);
+router.get('/', (req, res) => {
+  res.send('Welcome to the API');
+});
+
+router.get('/dashboard', authMiddleware, dashboard);
 router.post('/login', login);
 
 module.exports = router;
